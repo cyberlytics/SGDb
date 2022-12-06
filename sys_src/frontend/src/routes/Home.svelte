384e05f51f199d7ea88e601d2069992d066704d6
Philipp Stangl <p.stangl1@oth-aw.de>
2022-12-06 22:39:09 +0100

    Fetch from backend
    Former-commit-id: 208ef4490c0e36eab49b0a2d1c35b77b09453f66

--- sys_src/frontend/src/routes/Home.svelte
+++ sys_src/frontend/src/routes/Home.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { onMount } from "svelte";
   import { navigate } from "svelte-routing";
 
   import Graph from "../lib/Graph.svelte";
@@ -7,6 +8,12 @@
   const handleNodeClick= async (event: CustomEvent<string>) => {
     navigate(`/game/${event.detail}`);
   }
+
+  onMount(async () => {
+    const data = await fetch("http://localhost:8000/")
+    const json = await data.json();
+    console.log(json);
+  });
 </script>
 
 <Navbar />
