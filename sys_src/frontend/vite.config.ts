ec01faaf80464a73274c8f49b3040dbc37245266
Philipp Stangl <p.stangl1@oth-aw.de>
2022-12-07 11:44:35 +0100

    Configure path aliases
    Former-commit-id: 06c254a4045ee3520c934ead70abdd182249abe3

--- sys_src/frontend/vite.config.ts
+++ sys_src/frontend/vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve:{
+    alias:{
+      $lib: resolve(__dirname, 'src/lib'),
+      $components: resolve(__dirname, './src/lib/components'),
+      $utils: resolve(__dirname, './src/lib/utils'),
+    },
+  },
   plugins: [svelte()],
   server: {
     port: 8080
