51b03204d5a39003cb661012c7671805400bf161
Jakob Götz <j.goetz2@oth-aw.de>
2022-12-15 18:05:12 +0100

    requirements added
    Former-commit-id: 1fe3ddd0725d05848df36c0a5b04cfc55d472c5f

--- sys_src/backend/main.py
+++ sys_src/backend/main.py
@@ -57,7 +57,8 @@ def search():
 def detailpage(game: str):
     return json.dumps(detailpage_content(graph, game))
 
-
+'''
 # debugging purpose
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8810)
+'''
