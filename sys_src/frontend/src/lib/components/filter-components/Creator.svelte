e9210d83a439f9026c047ad77039390e14b153eb
Anastasia Chernysheva <a.chernysheva@oth-aw.de>
2022-12-19 02:18:37 +0100

    Added frontend-filter
    Former-commit-id: 2149e2539c0d1ee9707232b798f56b279aea6c44

---
+++ sys_src/frontend/src/lib/components/filter-components/Creator.svelte
@@ -0,0 +1,38 @@
+<script>
+    import Tags from "./Tags.svelte";
+    let group = [];
+    let creators = ['SasaGames', 'LalaGames', 'MoserGames', 'LoloGames']
+</script>
+<div class="left">
+    <h2>Creator</h2>
+    <div class='filter_container'>
+        {#each creators as creator}
+            <label class="border-zinc-900 text-sm text-gray-900">
+                <input type=checkbox value={creator} bind:group>
+                {creator}<br />
+            </label>
+        {/each}
+    </div>
+</div>
+
+    {#if group!=undefined}
+        {#each group as tag}
+            <Tags bind:tag={tag} bind:group={group}/>
+        {/each}
+    {/if}
+<style>
+    .left{
+        float: left;
+    }
+    h2 {
+        color: rgb(30, 29, 29);
+    }
+    .filter_container{
+        margin-right: 1em;
+        border: 2px solid #ccc;
+        width: 180px;
+        height: 98px;
+        overflow-y: scroll;
+        text-align: left;
+    }
+</style>
---
+++ sys_src/frontend/src/lib/components/filter-components/Tags.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let tag;
+    export let group = [];
+    function removeTag(tag){
+        group = group.filter(i => i !== tag);
+    }
+</script>
+
+<span class="input-tag">{tag}</span>
+<button type="button" class="remove-tag" on:click={() => removeTag(tag)}>&times;</button>
+
+
+<style>
+ .input-tag {
+        display: inline-flex;
+        line-height: 1.2;
+        align-items: center;
+        font-size: .875rem;
+        background-color: #911a1a;
+        color: #f8f4f4;
+        border-radius: 7px;
+        user-select: none;
+        padding: .5rem;
+        margin-right: .2rem;
+        margin-bottom: .25rem;
+        cursor: pointer;
+    }
+    .remove-tag{
+        color: #911a1a;
+        font-size: 1.125rem;
+        line-height: 1;
+    }
+</style>
