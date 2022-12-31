f05e521f7abf2b59d141ba6c0872054f3db289df
Jakob Götz <j.goetz2@oth-aw.de>
2022-12-31 16:39:21 +0100

    rework db_filter and filter api, see #35
    Former-commit-id: 6cd95a7fd2d6564316a3d7065c54d6a124477df1

--- sys_src/backend/db_filter.py
+++ sys_src/backend/db_filter.py
@@ -9,10 +9,6 @@ sparql_obj.setReturnFormat(JSON)
 
 """Create multiple filter queries so it's easy to combine them."""
 
-def convert_input(input):
-    """Convert the input string(s) in the list to url encoding to make it workable for the query"""
-    return [quote(i) for i in input]
-
 def combine_Filter(filter_requests):
 
     """Select the filters that should be combined for the query.
@@ -20,8 +16,8 @@ def combine_Filter(filter_requests):
     If new filters should be adapted or filters removed, pass the whole query object again to this method.
     Saves every game in the list that matches all filters.
     Matches Substrings and it is case insensitive"""
-    
-    game_list = []
+
+    game_list = {}
 
     if "date" in filter_requests:
         if "recommendation" not in filter_requests:
@@ -30,134 +26,142 @@ def combine_Filter(filter_requests):
             pass
     if "genre" in filter_requests:
         if "recommendation" not in filter_requests:
-            game_list = extend_list(game_list, fil_genre(filter_requests["genre"]))
+            for element in range(len(filter_requests["genre"])):  
+                game_list = extend_list(game_list, fil_genre(filter_requests["genre"][element-1]))
         else: 
-            game_list.append(fil_genre(filter_requests["genre"]))
+            game_list.update(fil_genre(filter_requests["genre"]))
     if "rating_num" in filter_requests:
         if "recommendation" not in filter_requests:
             game_list = extend_list(game_list, fil_rating(filter_requests["rating_num"]))
         else: 
-            game_list.append(fil_rating(filter_requests["rating_num"]))
+            game_list.update(fil_rating(filter_requests["rating_num"]))
     if "creator" in filter_requests:
         if "recommendation" not in filter_requests:
-            game_list = extend_list(game_list, fil_creator(filter_requests["creator"]))
+            for element in range(len(filter_requests["creator"])):  
+                game_list = extend_list(game_list, fil_creator(filter_requests["creator"][element-1]))
         else: 
-            game_list.append(fil_creator(filter_requests["creator"]))
+            game_list.update(fil_creator(filter_requests["creator"]))
     if "platform" in filter_requests:
         if "recommendation" not in filter_requests:
-            game_list = extend_list(game_list, fil_platform(filter_requests["platform"]))
+            for element in range(len(filter_requests["platform"])):  
+                game_list = extend_list(game_list, fil_platform(filter_requests["platform"][element-1]))
         else: 
-            game_list.append(fil_platform(filter_requests["platform"]))
+            game_list.update(fil_platform(filter_requests["platform"]))
     return game_list
+
 
 def extend_list(game_list, func_res, recommendation=False):
     """Only returns games that were already included in the results of other filters"""
     if(func_res):
+        func_res = func_res["results"]["bindings"]
         if len(game_list) != 0:
-            # If only exact matches should be returned
-            if not recommendation:
-                # return only the intersections of the game_list and the results of the specific filter
-                intersec = list(set(game_list).intersection(func_res))
-                if intersec:
-                    return intersec
-                else:
-                    return [None] # TODO maybe return something more meaningful
+            if game_list[0] is not None:
+                # If only exact matches should be returned
+                if not recommendation:
+                    intersec = []
+                    for i in game_list:
+                            for k in func_res:
+                                if i["title"]["value"] == k["title"]["value"] and i not in intersec: 
+                                    intersec.append(i)
+                                    break
+                    if intersec:
+                        return intersec
+                    else:
+                        return [None]
             # If recommendations should be returned
             if recommendation:
                 return get_biggest_intersec(game_list)
+
         else:
             # if the list is empty fill it with the first results 
             return func_res 
     else:
         # If the result is None return None
-        return [None]
+        return ["There is no game in the Database for the used Filter"]
+
 
 def get_biggest_intersec(game_list):
     """Find the game which fits the other game the best"""
     return Counter(game_list).most_common()
 
+
 def fil_date(year): 
     """Filter the games by the release year."""
     sparql_obj.setQuery("""
         PREFIX schema: <https://schema.org/>
-        SELECT ?game WHERE {{ 
-        ?game schema:releaseDate ?year .
+        SELECT ?title ?date WHERE {{ 
+        ?o schema:releaseDate ?year .
+        ?o schema:title ?title .
+        ?o schema:releaseDate ?date .
         FILTER (YEAR(?year) = {year})
         }}
         """.format(year=year))
-    return extract_res(sparql_obj)
+    return sparql_obj.queryAndConvert()
     
+
 def fil_genre(genre): 
     """Filter the games by the given genre(s)"""
-    genre = convert_input(genre)
-    fil_str = create_filter("?genre", genre)
+    genre = quote(genre)
+    fil_str = (f"FILTER REGEX(str(?genre), '{genre}', 'i') . ")
 
     sparql_obj.setQuery("""
         PREFIX schema: <https://schema.org/>
-        SELECT ?game WHERE {{ 
-        ?game schema:genre ?genre .
+        SELECT ?title ?date WHERE {{ 
+        ?o schema:genre ?genre .
+        ?o schema:title ?title .
+        ?o schema:releaseDate ?date .
         {fil_str}
         }}
         """.format(fil_str=fil_str))
-    return extract_res(sparql_obj)
+    return sparql_obj.queryAndConvert()
     
+
 def fil_rating(rating_num):
     """Filter games by best score that are better than the given score number. 
     The results are descending"""
 
     sparql_obj.setQuery("""
         PREFIX schema: <https://schema.org/>
-        SELECT ?game WHERE {{
-        ?game schema:ratingValue ?rating .
+        SELECT ?title ?date WHERE {{
+        ?o schema:ratingValue ?rating .
+        ?o schema:title ?title .
+        ?o schema:releaseDate ?date .
         FILTER(?rating > "{rating_num}")
         }}
         ORDER BY DESC(?rating)
         """.format(rating_num=rating_num))
-    return extract_res(sparql_obj)
+    return sparql_obj.queryAndConvert()
   
+
 def fil_creator(creator): 
     """Filter the games by the producer"""
-    creator = convert_input(creator)
-    fil_str = create_filter("?creator", creator)
+    creator = quote(creator)
+    fil_str = (f"FILTER REGEX(str(?creator), '{creator}', 'i') . ")
 
     sparql_obj.setQuery("""
         PREFIX schema: <https://schema.org/>
-        SELECT ?game WHERE {{
-        ?game schema:creator ?creator .
+        SELECT ?title ?date WHERE {{
+        ?o schema:creator ?creator .
+        ?o schema:title ?title .
+        ?o schema:releaseDate ?date .
         {fil_str}
         }}
         """.format(fil_str=fil_str))
-    # FILTER (lcase(str(?o)) = "red")
-    return extract_res(sparql_obj)
+    return sparql_obj.queryAndConvert()
    
 
 def fil_platform(platform):
     """Filter the games by their platform(s). Like ['Nintendo', 'Sony']."""
-    platform = convert_input(platform)
-    fil_str = create_filter("?platform", platform)
+    platform = quote(platform)
+    fil_str = (f"FILTER REGEX(str(?platform), '{platform}', 'i') . ")
 
     sparql_obj.setQuery("""
         PREFIX schema: <https://schema.org/>
-        SELECT ?game WHERE {{
-        ?game schema:gamePlatform ?platform .
+        SELECT ?title ?date WHERE {{
+        ?o schema:gamePlatform ?platform .
+        ?o schema:title ?title .
+        ?o schema:releaseDate ?date .
         {fil_str}
         }}
         """.format(fil_str=fil_str))
-    return extract_res(sparql_obj)
-
-def create_filter(type, arg_list):
-    """Create a Query string for a name search filter method by n given arguments
-    and returns a n long Filter as a string"""
-    fil_str= ""
-    for arg in arg_list:
-        fil_str += (f"FILTER REGEX(str({type}), '{arg}', 'i') . ")
-    return fil_str
-
-def extract_res(result):
-    """get the games themself as subjects inside a list"""
-    try:
-        ret = result.queryAndConvert()
-        games = ret["results"]["bindings"]
-        return [game["game"]["value"] for game in games]
-    except Exception as e:
-        print(e)
+    return sparql_obj.queryAndConvert()
--- sys_src/backend/main.py
+++ sys_src/backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from db_wrapper import detailpage_content, search_query, get_root_graph, query_the_subject
+from db_wrapper import detailpage_content, search_query, get_root_graph
 from db_filter import combine_Filter
 import json
 from filter_lists import get_data
@@ -24,8 +24,8 @@ app.add_middleware(
 # returns a root-graph in dependency to the release-date of a game
 @app.get("/")
 def startpage():
-    root_graph = get_root_graph()
     root = {'data': {}, 'filters': {}}
+    root_graph = get_root_graph()
     for year in range(1985, 2023):
         title_in_year = []
         for i in range(len(root_graph["results"]["bindings"])):
@@ -44,34 +44,53 @@ def startpage():
 
 @app.post("/")
 def startpage(filter_requests: dict):
-    # only keep dict keys with values inside
-    for set_filter in filter_requests:
-        if set_filter == "":
-            del filter_requests[set_filter]
+    # check if the filter is only one date and rating
+    if "date" in filter_requests:
+        if not isinstance(filter_requests["date"], int):
+                return JSONResponse(
+                status_code=404,
+                content={"message": "only Filter one year"},
+            )
+    if "rating_num" in filter_requests:
+        if not isinstance(filter_requests["rating_num"], int):
+                return JSONResponse(
+                status_code=404,
+                content={"message": "only Filter one rating_num"},
+            )
 
-    filter_graph_iri = combine_Filter(filter_requests)
+    root = {'data': {}, 'filters': {}}
+    root_graph = combine_Filter(filter_requests)
 
-    filtered_games = []
-    for i in filter_graph_iri:
-        filtered_iri = query_the_subject(i)
-        filtered_iri = filtered_iri["results"]["bindings"]
-        filtered_games.append(filtered_iri)
+    if root_graph is None:
+            return JSONResponse(
+            status_code=404,
+            content={"message": "no matching Game with the Filter"},
+        )
 
-    game_info = {}
-    for filtered_game in filtered_games:
-        game_date = ""
-        game_list = []
-        for k in range(len(filtered_game)):
-            if str(filtered_game[k]["predicate"]["value"]).find("title") != -1:
-                game_list.append(filtered_game[k]["object"]["value"])
-            if str(filtered_game[k]["predicate"]["value"]).find("Date") != -1:
-                game_date = filtered_game[k]["object"]["value"][0:4]
-        if game_date:
-            if game_date not in game_info:
-                game_info[game_date] = game_list
-            else:
-                game_info[game_date].extend(game_list)
-    return game_info
+    if None in root_graph:
+            return JSONResponse(
+            status_code=404,
+            content={"message": "no matching Game with the Filter"},
+        )
+
+    for game in root_graph:
+        year = game['date']['value'][:4]  # Extract the year from the "date" field
+        title = game['title']['value']  # Extract the game title from the "title" field
+
+        # If the year is not already a key in the dictionary, create a new key-value pair
+        # with the year as the key and an empty list as the value
+        if year not in root['data']:
+          root['data'][year] = []
+
+        # Append the game title to the list of game titles for the current year
+        root['data'][year].append(title)
+
+    # add filter names to startpage
+    filter_data = get_data()
+    for data in filter_data:
+        root['filters'].update(data)
+
+    return(root)
 
 
 # search request
