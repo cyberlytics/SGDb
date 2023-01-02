6be03a6b404a4cc819b9e36a4538cda7d3e2aa7f
Anastasia Chernysheva <a.chernysheva@oth-aw.de>
2023-01-02 23:54:45 +0100

    Scrolling disabled and changed tag errors
    Former-commit-id: b67c3ff9f1838f818757f71bcd20093149de8d8b

--- sys_src/frontend/src/lib/components/Filter.svelte
+++ sys_src/frontend/src/lib/components/Filter.svelte
@@ -1,72 +1,55 @@
 <script>
-    import { onMount } from "svelte";
     import Creator from "./filter-components/Creator.svelte";
     import Genre from "./filter-components/Genre.svelte";
     import Platform from "./filter-components/Platform.svelte";
     import Date from "./filter-components/Date.svelte";
-    import Tags from "./filter-components/Tags.svelte"
+    import Tags from "./filter-components/Tags.svelte";
+    import {filterSettings} from '$stores/filter.ts';
 
     let creator;
-    let num;
-    let genre;
-    let platform;
+    let platform=[];
+    let genre=[];
+    let date;
 
-    
     let genre_color= 'rgb(90, 73, 157)';
     let creator_color ='rgb(66, 153, 63)';
     let platform_color ='rgb(73, 122, 157)';
     let date_color ='rgb(176, 55, 55)';
 
-    let data_genre = [];
-    let data_platform = [];
-    let data_creator = [];
+    let filter;
+    $: filter = JSON.stringify({creator, platform, genre, date});
+    $: filterSettings.set(filter);
     
-    function toArray(data, array){
-        array = []
-        for (var key in data){
-            for(var k in data[key]){
-                array.push( k );
-            }
-        }
-        console.log(array);
-        return array;
+    $: if(creator==undefined && platform.length==0 && genre.length==0 && date==undefined){
+        filterSettings.set('');
     }
 
-    onMount(async function () {
-        const res = await fetch('http://localhost:8000/', {
-			method: 'GET'
-		})
-        let data = await res.json();
-        data_genre = toArray(data.filters.genre, data_genre);
-        data_platform = toArray(data.filters.platform, data_platform);
-        data_creator = toArray(data.filters.creator, data_creator);
-    });
-    //passes set filteritems to backend with post-method dynamicly
-    async function postObj () {
-		const res = await fetch('https://httpbin.org/post', {
-			method: 'POST',
-			body: JSON.stringify({creator,genre,platform,num})
-		})
-		await res.json()
-	}
+    if($filterSettings!=''){
+        let settings = JSON.parse($filterSettings);
+        creator= settings.creator;
+        platform= settings.platform;
+        genre= settings.genre;
+        date= settings.date;
+    }
 </script>
 <!--Modal contains filter options and set filteritem tags-->
-<div class="filter_box" on:change={postObj}>
+<div class="filter_box">
     <div class="position">
         <!--Creates new filteroptions, returns checked items as values in a list 
         and passes color-variables for style-->
-        <Genre bind:group={genre} bind:color={genre_color} bind:data={data_genre}/>  
-        <Platform bind:group={platform} bind:color={platform_color} bind:data={data_platform}/>
-        <Creator bind:group={creator} bind:color={creator_color} bind:data={data_creator}/>    
-        <Date bind:num={num} bind:color={date_color}/>
+        <Genre bind:group={genre} bind:color={genre_color} />  
+        <Platform bind:group={platform} bind:color={platform_color}/>
+        <Creator bind:group={creator} bind:color={creator_color}/>    
+        <Date bind:date={date} bind:color={date_color}/>
     </div>
     <div class="tags">
          <!--Creates new tags as checked, passes meantfor colors and values-->
-        <Tags bind:group={creator} bind:color={creator_color}/>
+        <Tags bind:creator={creator} bind:color={creator_color}/>
         <Tags bind:group={genre} bind:color={genre_color}/>
         <Tags bind:group={platform} bind:color={platform_color}/>
-        <Tags bind:num={num} bind:color={date_color}/>
+        <Tags bind:date={date} bind:color={date_color}/>
     </div>
+
 </div>
 <style>
     .position{
@@ -74,16 +57,16 @@
     }
     .filter_box {
 		width: 69em;
-        height: 11em;
-        border: 1px solid rgb(47, 47, 47);
-        border-radius: 7px;
-        box-shadow: 2px 2px 8px rgba(145, 65, 65, 0.1);
-        padding: 2em;
-        margin-left: 1.5em;
-        margin-top: 5em;
-        
+        margin-top: 2em;
+        margin-bottom: 2em;
 	}
     p{
         color: rgb(30, 29, 29); 
+    }
+    .tags {
+        margin-left: 47em;
+        width: 20em;
+        height: 7em;
+        overflow-y: auto;
     }
 </style>
