3ceff86dc4286dc6a95404f1b52a694dca25e08a
Philipp Stangl <p.stangl1@oth-aw.de>
2023-01-08 01:03:39 +0100

    fix: Hide search suggestions when user clicked search button
    Former-commit-id: a0c4c3caf0fcd3a38815c333514ed5388b9da64e

--- sys_src/frontend/src/lib/stores/search.ts
+++ sys_src/frontend/src/lib/stores/search.ts
@@ -1,6 +1,6 @@
-import { writable, derived } from 'svelte/store';
+import { writable } from 'svelte/store';
 
 export const searchQuery = writable('');
 export const searchText = writable('');
 export const isSearchDisabled = writable(true);
-export const isSearchSuggestionVisible = derived([isSearchDisabled], ([$isSearchDisabled]) => !$isSearchDisabled);
+export const isSearchSuggestionVisible = writable(false);
