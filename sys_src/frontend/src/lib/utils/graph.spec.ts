5dd5b658c592e4c8f096c713c8aa48569f903e56
Philipp Stangl <p.stangl1@oth-aw.de>
2023-01-16 15:14:20 +0100

    fix: Resolve conflict in graph.spec.ts
    Former-commit-id: fbc2b0f5eddafd5bb3bb805d2076e6d2c36ec9d4

--- sys_src/frontend/src/lib/utils/graph.spec.ts
+++ sys_src/frontend/src/lib/utils/graph.spec.ts
@@ -1,10 +1,14 @@
 import { buildGraph,  buildStartPageGraph} from './graph';
 
 describe("graph", () => {
+
   it("builds an empty graph", async () => {
     const graph = await buildGraph([], []);
     expect(graph).toBeDefined();
   });
+
+  const object = {data: {2000:['test1', 'test2'], 2001:['test1', 'test2']}, filter: {}}
+
   it("builds startpagegraph", async () => {
     const startpagegraph = await buildStartPageGraph(object);
     expect(startpagegraph).toBeDefined();
